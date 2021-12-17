15d8eb00462b33d4b1459171b68f0cf20e84dc55
Gregory Vettorel <43611326+gregoryvettorel2@users.noreply.github.com>
2021-12-17 01:50:39 +0100

    Graph.js added, working on coin page

--- src/index.js
+++ src/index.js
@@ -3,42 +3,66 @@ var proxyUrl = "https://cors-anywhere.herokuapp.com/";
 var apiKey = "4f508b14-de1b-4bce-9b14-821a68f239b4";
 
 //GRAPH SHIZZLES
-const ctx = document.getElementById('myChart').getContext('2d');
+async function createChart(){
+console.log("chart generated.");
+const ctx = await document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['-24h','-23h','-22h','-21h','-20h','-19h','-18h','-17h','-16h','-15h','-14h','-13h','-12h','-11h','-10h','-9h','-8h','-7h','-6h','-5h','-4h','-3h','-2h','-1h', 'Now'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: '24h price movement',
+            data: [
+              "775.6715646572332397640000",
+              "766.0828736988712539770000",
+              "754.3738755574379994010000",
+              "752.7211896665261544310000",
+              "757.0374186287868010470000",
+              "757.6769305682970088350000",
+              "805.6753372007194317140000",
+              "805.0983768457222427550000",
+              "799.8746616469092196570000",
+              "802.6523394071007624980000",
+              "797.1531568318142140090000",
+              "772.9244953330569364850000",
+              "764.5060492057627600830000",
+              "771.0263864348801335100000",
+              "773.8714239213700431810000",
+              "785.3785343267603631110000",
+              "793.4483197223557615750000",
+              "803.0993985364754719390000",
+              "824.7814978202695026330000",
+              "833.5786423557748255170000",
+              "824.5928048488282835530000",
+              "817.8797765565419878640000",
+              "828.6137027034925471120000",
+              "855.8610227474447877480000",
+              "853.2232587985660993050000",
+              "867.1864718882274791380000",
+              "870.5260037488412452620000"
+          ],
+      
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(0, 2, 252)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(0, 2, 252)'
             ],
             borderWidth: 1
         }]
     },
     options: {
+      maintainAspectRatio: false,
         scales: {
             y: {
-                beginAtZero: true
+                beginAtZero: false
             }
         }
     }
 });
+}
 
+//GET DATA NIZZLES
 async function getIndexData() {
   var apiUrl = `${proxyUrl}${baseUrl}`;
   console.log(apiUrl);
@@ -75,7 +99,6 @@ async function getIndexData() {
   for(let i = 0; i < 4; i++){
     changeColor = "";
     let changeVariable = topGainersCoins[i].change;
-    console.log(changeVariable);
     if(changeVariable.charAt(0) == "-"){
       changeColor += "red";
     } else {
@@ -126,4 +149,6 @@ async function getIndexData() {
   document.getElementById("50coins").innerHTML = cryptoCoin;
 }
 
+//Calling functions
 getIndexData();
+createChart();
